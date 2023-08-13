ac5b939fc653962b01e8e4cf2113f4bf58496918
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-08-13 14:40:02 -0400

    cleaned up code and comments

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 const db = require('./db/employee_db.js');
 const cTable = require('console.table');
 
-//had to integrate this goHome function below here because it was not working in the index.js file the way I wanted it to show in the terminal and I couldn't require index.js in the queries.js file because it was causing a circular dependency.  (That was a good lesson to learn just took me about an hour to figue it out!  Now I know what a circular dependency is and how to avoid it!)
+//had to integrate this goHome function below here because it was not working in the index.js file the way I wanted it to show in the terminal and I couldn't require index.js in the queries.js file because it was causing a circular dependency.     
 
 //All the functions below have self-eplanitory names so I'm not commenting on all of them.  There is a queries.sql file where I kept the SQL functions for reference after testing in the shell.  Then I copied and pasted in the template literals below.
 
@@ -34,7 +34,8 @@ module.exports = function queries(goHome) {
                 goHome();
             })
         },
-//this is an attempt at the bonus function to see the salaries budget of each department.  I don't think it's summing the salaries.  I think it's just showing the salary of the first role in each department.  When I have time I will go back to fix it.  I'm not sure if I need to use a JOIN or not.
+//this is an attempt at the bonus function to see the salaries budget of each department.  I don't think it's summing the salaries.  I think it's just showing the salary of the first role in each department.  When I have time I will go back to fix it.   
+
         viewBudgets() {
             db.query(`SELECT department.deptname AS Department, SUM(role.salary) AS Budget FROM department INNER JOIN role ON department.id = role.department_id GROUP BY deptname;`, (err, res) => {
                 if (err) throw err;
